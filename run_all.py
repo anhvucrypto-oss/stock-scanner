521daa64ed1159db8b31c5fc700b1435cd6a18e7
anhvucrypto-oss <anhvucrypto@gmail.com>
2026-04-13 15:00:00 +0700

    Update run_all.py

--- run_all.py
+++ run_all.py
@@ -3,7 +3,7 @@ import subprocess
 print("🚀 START BOT + DASHBOARD")
 
 # chạy bot
-subprocess.Popen(["python", "auto_trade_final.py"])
+subprocess.Popen(["python", "auto_trade_meta_ai.py"])
 
 # chạy dashboard
 subprocess.Popen(["streamlit", "run", "dashboard.py"])
